af3998172c108d2c57b13870f94633cd8817c908
Shadi <shadizabad1@gmail.com>
2021-04-29 13:35:44 -0400

    Adding HM3 filter file

--- starter_script.sh
+++ starter_script.sh
@@ -1,19 +1,11 @@
 # Initialization script that sets up the compute environment on ComputeCanada
 # You may skip some of these steps if you have a different working environment
 
-module load python/3.7
-module load scipy-stack
+cd "$HOME/projects/def-sgravel/szabad/viprs-paper" || exit
 
-virtualenv --no-download $SLURM_TMPDIR/env
-source $SLURM_TMPDIR/env/bin/activate
-
-cd /home/szabad/projects/def-sgravel/szabad/viprs-paper || exit
-
-pip install --no-index --upgrade pip
-pip install --no-index -r requirements.txt
-pip install --no-index -e ./software/rechunker
+source "$HOME/pyenv/bin/activate"
 
 cd gwasimulator || exit
 python setup.py
 
-cd /home/szabad/projects/def-sgravel/szabad/viprs-paper || exit
+cd "$HOME/projects/def-sgravel/szabad/viprs-paper" || exit
