63d7de99fc486f1c02cd3b00b4fd5811232977bd
Shadi <shadizabad1@gmail.com>
2021-06-17 13:25:00 -0400

    Adding a batch plot script

---
+++ plotting/batch_plot.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+source "$HOME/pyenv/bin/activate"
+
+python plotting/plot_hyperparameter_estimates.py -l ukbb_50k_windowed
+python plotting/plot_hyperparameter_estimates.py -l ukbb_50k_sample
+python plotting/plot_hyperparameter_estimates.py -l ukbb_50k_shrinkage
+python plotting/plot_predictive_performance.py -l ukbb_50k_shrinkage
+python plotting/plot_predictive_performance.py -l ukbb_50k_sample
+python plotting/plot_predictive_performance.py -l ukbb_50k_windowed
+python plotting/plot_time_stats.py 
