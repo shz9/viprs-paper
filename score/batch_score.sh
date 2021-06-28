afbe2dd2ce4a047d1e0ecd24b5f0a55c343f692a
Shadi <shadizabad1@gmail.com>
2021-06-28 02:28:04 -0400

    Adding scoring as a separate step, separate from evaluation.

--- score/batch_score.sh
+++ score/batch_score.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
 
 ld_panel=${1:-"ukbb_50k_windowed"}
+model=${2:-"VIPRS"}
 
 echo "Submitting jobs for generating per-chromosome polygenic scores."
-echo "Processing data for models using $ld_panel LD panel..."
+echo "Model: $model"
+echo "LD panel: $ld_panel"
 
-if [ -z "$2" ]; then
+if [ -z "$3" ]; then
   echo "Generating polygenic scores for all configurations..."
   input_dir=($(ls -d data/gwas/* | xargs -n 1 basename))
 else
-  echo "Generating polygenic scores for phenotypes in $2 ..."
-  input_dir=($2)
+  echo "Generating polygenic scores for phenotypes in $3 ..."
+  input_dir=($3)
 fi
 
 for indir in "${input_dir[@]}"
 do
 
-  rm -rf "./log/score/$ld_panel/$indir" || true
-  mkdir -p "./log/score/$ld_panel/$indir"
+  rm -rf "./log/score/$ld_panel/$model/$indir" || true
+  mkdir -p "./log/score/$ld_panel/$model/$indir"
 
-  for fit_file in data/model_fit/"$ld_panel"/*/"$indir"/*/*.fit
+  for fit_file in data/model_fit/"$ld_panel"/"$model"/"$indir"/*/*.fit
   do
-    sbatch -J "$ld_panel/$indir" evaluation/evaluation_job.sh "$fit_file"
+    sbatch -J "$ld_panel/$model/$indir" score/score_job.sh "$fit_file"
   done
 
 done
