3f34845f57c56d60c4bf37b4fb5b8f1c92d55a2b
Shadi <shadizabad1@gmail.com>
2021-07-31 10:54:34 -0400

    Updating SBayesR sumstats conversion script

--- external/SBayesR/sumstats_to_ma.py
+++ external/SBayesR/sumstats_to_ma.py
@@ -10,15 +10,13 @@ parser.add_argument('-t', '--type', dest='type', type=str, default='plink',
                     choices={'plink', 'pystatgen'})
 args = parser.parse_args()
 
+# Read the sumstats file:
+ss_df = pd.read_csv(args.ss_file, sep="\t")
+
 if args.type == 'pystatgen':
-    ss_df = pd.read_csv(args.ss_file, sep="\t")
     ss_df = ss_df[['SNP', 'A1', 'A2', 'MAF', 'BETA', 'SE', 'PVAL', 'N']]
 elif args.type == 'plink':
-
-    ss_df = pd.read_csv(args.ss_file, sep="\s+")
-    snp_var = 1. / (ss_df['OBS_CT'] * ss_df['SE'] ** 2)
-    ss_df['MAF'] = -.5 * (np.sqrt(1. - 2. * snp_var) - 1.)
-    ss_df = ss_df[['ID', 'ALT', 'REF', 'MAF', 'BETA', 'SE', 'P', 'OBS_CT']]
+    ss_df = ss_df[['ID', 'ALT1', 'REF', 'A1_FREQ', 'BETA', 'SE', 'P', 'OBS_CT']]
 
 # Write the results to the same directory:
 ss_df.columns = ['SNP', 'A1', 'A2', 'freq', 'b', 'se', 'p', 'n']
