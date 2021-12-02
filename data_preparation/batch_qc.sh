c1c5e655332d5d1a72dc686226de85b3257dc126
Shadi <shadizabad1@gmail.com>
2021-12-02 02:01:52 -0500

    Making qc scripts more flexible.

--- data_preparation/batch_qc.sh
+++ data_preparation/batch_qc.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
 
-rm -rf ./log/data_preparation/ukbb_qc/ || true
-mkdir -p ./log/data_preparation/ukbb_qc/
+data_source=${1:-"ukbb"}
+snp_set=${2:-"hm3"}
 
-rm -rf ./log/data_preparation/1000G_qc/ || true
-mkdir -p ./log/data_preparation/1000G_qc/
+if [ $data_source == "ukbb" ]; then
+  if [ $snp_set == "hm3" ]; then
+    log_dir="log/data_preparation/ukbb_qc/hm3/"
+  else
+    log_dir="log/data_preparation/ukbb_qc/all/"
+  fi
+  qc_script="data_preparation/ukbb_qc_job.sh"
+else
+  if [ $snp_set == "hm3" ]; then
+    log_dir="log/data_preparation/1000G_qc/hm3/"
+  else
+    log_dir="log/data_preparation/1000G_qc/all/"
+  fi
+  qc_script="data_preparation/1000G_qc_job.sh"
+fi
+
+rm -rf "$log_dir" || true
+mkdir -p "$log_dir"
 
 echo "Submitting QC jobs..."
 
 for c in $(seq 1 22)
 do
-  sbatch data_preparation/ukbb_qc_job.sh "$c"
-  sbatch data_preparation/1000G_qc_job.sh "$c"
+  sbatch -J "$log_dir" "$qc_script" "$c" "$snp_set"
 done
-
--- data_preparation/ukbb_qc_job.sh
+++ data_preparation/ukbb_qc_job.sh
@@ -13,16 +13,26 @@ echo "Job ID: $SLURM_JOBID"
 module load plink
 
 cd /home/szabad/projects/def-sgravel/szabad/viprs-paper || exit
-mkdir -p "data/ukbb_qc_genotypes/"
 
 CHR=${1:-22}  # Chromosome number (default 22)
+snp_set=${2:-"hm3"} # The SNP set to use
+
+if [ $snp_set == "hm3" ]; then
+  snp_keep="data/keep_files/ukbb_qc_variants_hm3.keep"
+  output_dir="data/ukbb_qc_genotypes"
+else
+  snp_keep="data/keep_files/ukbb_qc_variants.keep"
+  output_dir="data/ukbb_qc_genotypes_all"
+fi
+
+mkdir -p "$output_dir"
 
 plink2 --bgen "/lustre03/project/6004777/projects/uk_biobank/imputed_data/full_UKBB/v3_bgen12/ukb_imp_chr${CHR}_v3.bgen" \
       --sample "/lustre03/project/6004777/projects/uk_biobank/imputed_data/full_UKBB/v3_bgen12/ukb6728_imp_chr${CHR}_v3_s487395.sample" \
       --make-bed \
       --allow-no-sex \
       --keep data/keep_files/ukbb_qc_individuals.keep \
-      --extract data/keep_files/ukbb_qc_variants_hm3.keep \
+      --extract "$snp_keep" \
       --hwe 1e-10 \
       --mind 0.05 \
       --geno 0.05 \
@@ -30,16 +40,16 @@ plink2 --bgen "/lustre03/project/6004777/projects/uk_biobank/imputed_data/full_U
       --snps-only \
       --max-alleles 2 \
       --hard-call-threshold 0.1 \
-      --out "data/ukbb_qc_genotypes/chr_${CHR}"
+      --out "$output_dir/chr_${CHR}"
 
 module load nixpkgs/16.09
 module load plink/1.9b_4.1-x86_64
 # Update the SNP cM position using the HapMap3 genetic map:
-plink --bfile "data/ukbb_qc_genotypes/chr_${CHR}" \
+plink --bfile "$output_dir/chr_${CHR}" \
       --cm-map "$HOME/projects/def-sgravel/data/genetic_maps/1000GP_Phase3/genetic_map_chr@_combined_b37.txt" \
       --make-bed \
-      --out "data/ukbb_qc_genotypes/chr_${CHR}"
+      --out "$output_dir/chr_${CHR}"
 
-rm -r data/ukbb_qc_genotypes/*~
+rm -r "$output_dir"/*~
 
 echo "Job finished with exit code $? at: `date`"
